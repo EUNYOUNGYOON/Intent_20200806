a641d0a2b56a4c0e7d38982a46e86818371e929c
EUNYOUNGYOON <milkoon1@naver.com>
2020-08-10 19:38:48 +0900

    완료 버튼을 누르면 입력한 닉네임을 들고 finish

--- app/src/main/java/com/example/intent_20200806/EditNickNameActivity.kt
+++ app/src/main/java/com/example/intent_20200806/EditNickNameActivity.kt
@@ -1,5 +1,7 @@
 package com.example.intent_20200806
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_edit_nick_name.*
@@ -15,6 +17,16 @@ class EditNickNameActivity : AppCompatActivity() {
             //완료버튼을 눌렀다는것은 데이터를 들고 돌아가겠다 (확정)
             // 복귀 : finish()
             // 백버튼을 눌러서 종료하는 것과 구별해야한다.
+
+            // 돌아갈 때 첨부할 데이터를 들고 있어주는 Intent 별개로 생성
+            val resultIntent = Intent()
+
+            // 입력된 닉네임을 resultIntent에 첨부 - putExtra
+            val inputNickName = nickNameEdt.text.toString()
+            resultIntent.putExtra("newNickName", inputNickName)
+
+            // OK버튼을 눌렀다고 명시 + 데이터를 담고 있는 resultIntent를 갖고 복귀하게 처리.
+            setResult(Activity.RESULT_OK, resultIntent)
 
             finish()
         }
--- app/src/main/java/com/example/intent_20200806/SecondActivity.kt
+++ app/src/main/java/com/example/intent_20200806/SecondActivity.kt
@@ -3,11 +3,9 @@ package com.example.intent_20200806
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        //changeNickNameBtn.set
+        setContentView(R.layout.activity_second)
     }
 }
